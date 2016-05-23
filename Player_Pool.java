ede9242ff18736e807e2eb936a8779fbebda7111
Yagyaansh <yg6uk@virginia.edu>
2016-05-23 21:28:02 +0800

    Update Player_Pool.java

--- Player_Pool.java
+++ Player_Pool.java
@@ -31,20 +31,23 @@ public class Player_Pool {
 
 	}
 	
-	public void pickAlternator(ArrayList<Player> GMPicksTeam1,
-			ArrayList<Player> GMPicksTeam2, Player_Pool PlayerPool, GeneralManager GM_of_team1, GeneralManager GM_of_team2) { 
-		//General managers alternate between selecting of players in the PlayerPool
-		boolean bool = true;
-		for (int i = 0; i < 100; i++) {
-			if (bool == true) {
-				GMPicksTeam1.add(GM_of_team1.GeneralManagerPick(PlayerPool));
-				bool = false;
-			} else {
-				GMPicksTeam2.add(GM_of_team2.GeneralManagerPick(PlayerPool));
-				bool = true;
-			}
-		}
+	// method will not work since this is only for 2 teams
+	// now we can have an arbitrary number of teams
+	
+	// public void pickAlternator(ArrayList<Player> GMPicksTeam1,
+	// 		ArrayList<Player> GMPicksTeam2, Player_Pool PlayerPool, GeneralManager GM_of_team1, GeneralManager GM_of_team2) { 
+	// 	//General managers alternate between selecting of players in the PlayerPool
+	// 	boolean bool = true;
+	// 	for (int i = 0; i < 100; i++) {
+	// 		if (bool == true) {
+	// 			GMPicksTeam1.add(GM_of_team1.GeneralManagerPick(PlayerPool));
+	// 			bool = false;
+	// 		} else {
+	// 			GMPicksTeam2.add(GM_of_team2.GeneralManagerPick(PlayerPool));
+	// 			bool = true;
+	// 		}
+	// 	}
 
-	}
+	// }
 
 }
