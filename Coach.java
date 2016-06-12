d40da34a62066fd9df49325e769751fa5f11ff3f
Yagyaansh <yg6uk@virginia.edu>
2016-06-12 18:13:04 +0800

    Update Coach.java

--- Coach.java
+++ Coach.java
@@ -6,45 +6,69 @@ import java.util.Scanner;
 public class Coach {
 
 	private int scheme;
-	//private int favor; // guys whats this field ? Could not find it in the document
 	private int salary;
-	private String firstNameCoach;
-	private String lastNameCoach;
-	public ArrayList<Player> coachPicks;
+	private String firstName;
+	private String lastName;
 
-
-	// private Random ran = new Random();
-	
-	// the scheme will be a random value between 25 and 75. 
-	// removed it from the constructor
-
+	/*
+	 * This constructor initializes a coach using first name and last name
+	 * Scheme is determined in the constructor
+	 */
 	public Coach(String firstName, String lastName) {
-		this.firstNameCoach = firstName; // will always be set to "Coach"
-		this.lastNameCoach = lastName;
-		//this.favor=50;
+		this.firstName = firstName; 
+		this.lastName = lastName;
 		this.setScheme(getRandomScheme());
 		this.setSalary(1000000);
-		this.coachPicks=new ArrayList<Player>();
-
 	}
 	
+	/*
+	 * Initializes a constructor without parameters
+	 * Scheme is determined in the constructor
+	 */
+	public Coach() {
+		this.firstName = ""; 
+		this.lastName = "";
+		this.setScheme(getRandomScheme());
+		this.setSalary(0);
+	}
+
+	/*
+	 * Randomly generates a Scheme value for the Coach
+	 * Follows a uniform distribution between 25 and 75
+	 */
 	public int getRandomScheme()
 	{
-		// randomly generated
-		// uniform distribution between 25 and 75
-		
 		return (int)(Math.random()*(75-25))+25;
 	}
+	
+
+	/*
+	 * Printing methods
+	 * -------------------------------------------------------------------------------------------------
+	 */
+	
 
 	public void printCoach() {
 
 		System.out.print("Coach: Coach" /* + firstNameCoach + */+ " "
-				+ lastNameCoach + " , ");
+				+ lastName + " , ");
 		System.out.print("Scheme: " + this.scheme + " , ");
 		// System.out.print("Favor: " + this.favor+" , ");
 		System.out.println("Salary: $" + this.salary + ". ");
 
 	}
+	
+
+	/*
+	 * End of printing methods
+	 * -------------------------------------------------------------------------------------------------
+	 */
+	
+	
+	/*
+	 * Getters and setters for the fields in the class
+	 * -------------------------------------------------------------------------------------------------
+	 */
 
 	public int getScheme() {
 		return scheme;
@@ -54,14 +78,6 @@ public class Coach {
 		this.scheme = scheme;
 	}
 
-	/*public int getFavor() {
-		return favor;
-	}
-
-	public void setFavor(int favor) {
-		this.favor = favor;
-	}*/
-
 	public int getSalary() {
 		return salary;
 	}
@@ -69,19 +85,12 @@ public class Coach {
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-	
-	public void coachPickStarters(ArrayList<Player> GMPicks) {
-		//Coach chooses starters from the players selected by the GM
-		Random ranIndex = new Random();
-		for (int i = 0; i < 22; i++) {
-			int index = ranIndex.nextInt(GMPicks.size());
-			coachPicks.add(GMPicks.get(index));
-			GMPicks.remove(index);
-		}
-		// System.out.println(PlayerPool.getSize());
 
-	}
 	
+	/*
+	 * End of Getters and Setters
+	 * -------------------------------------------------------------------------------------------------
+	 */
 
 
 }
