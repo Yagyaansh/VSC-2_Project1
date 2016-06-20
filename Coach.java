d84bed47c8e29bf24ff94ac6b92854e9b423e444
Jeffery You <jyou543@gmail.com>
2016-06-20 04:36:56 -0500

    Update Coach.java

--- Coach.java
+++ Coach.java
@@ -1,104 +1,57 @@
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 
-public class Coach {
-
-	private int scheme;
-	private int salary;
-	private String firstName;
-	private String lastName;
+public class Coach_Pool {
 
 	/*
-	 * This constructor initializes a coach using first name and last name
-	 * Scheme is determined in the constructor
+	 * All the coaches in list form
+	 * See if we should use a better data structure
+	 * The array list is too inefficient for search
 	 */
-	public Coach(String firstName, String lastName) {
-		this.firstName = firstName; 
-		this.lastName = lastName;
-		this.setScheme(getRandomScheme());
-		this.setSalary(1000000);
+	private ArrayList<Coach> CoachesPool = new ArrayList<Coach>();
+
+	/*
+	 * Initializes the pool given the size of the pool as input 
+	 * Name generator is used to assign last names
+	 * 1st name is always "Coach"
+	 */
+	public Coach_Pool(int sizeOfCoachesPool) {
+		try 
+		{
+			NameGenerator randomNames = new NameGenerator();
+			for (int i = 0; i < sizeOfCoachesPool; i++) 
+			{
+				CoachesPool.add(new Coach("Coach", randomNames.randomLastName()));
+			}
+		} catch (Exception e) {
+			System.out.println("Error encountered in Coach_Pool");
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
-	
-	/*
-	 * Initializes a constructor without parameters
-	 * Scheme is determined in the constructor
-	 */
-	public Coach() {
-		this.firstName = ""; 
-		this.lastName = "";
-		this.setScheme(getRandomScheme());
-		this.setSalary(0);
-	}
 
-	/*
-	 * Randomly generates a Scheme value for the Coach
-	 * Follows a uniform distribution between 25 and 75
-	 */
-	public int getRandomScheme()
-	{
-		return (int)(Math.random()*(75-25))+25;
-	}
-	
-
-	/*
-	 * Printing methods
-	 * -------------------------------------------------------------------------------------------------
-	 */
-	
-
-	public void printCoach() {
-
-		System.out.print("Coach: Coach" /* + firstNameCoach + */+ " "
-				+ lastName + " , ");
-		System.out.print("Scheme: " + this.scheme + " , ");
-		// System.out.print("Favor: " + this.favor+" , ");
-		System.out.println("Salary: $" + this.salary + ". ");
-
-	}
-	
-
-	/*
-	 * End of printing methods
-	 * -------------------------------------------------------------------------------------------------
-	 */
-	
 	
 	/*
 	 * Getters and setters for the fields in the class
 	 * -------------------------------------------------------------------------------------------------
 	 */
-
-	public int getScheme() {
-		return scheme;
+	public ArrayList<Coach> getCoachesPool() {
+		return CoachesPool;
 	}
 
-	public void setScheme(int scheme) {
-		this.scheme = scheme;
+	public void setCoachesPool(ArrayList<Coach> coaches_Pool) {
+		CoachesPool = coaches_Pool;
 	}
 
-	public int getSalary() {
-		return salary;
+	public int getSize() {
+		return CoachesPool.size();
 	}
-
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-
 	
 	/*
 	 * End of Getters and Setters
 	 * -------------------------------------------------------------------------------------------------
 	 */
-
-	public Coach deepCopy(){
-		Coach c = new Coach();
-		c.scheme=this.scheme;
-		c.salary=this.salary;
-		c.firstName=this.firstName;
-		c.lastName=this.lastName;
-		return c;
-	}
+	
+	
 
 }
