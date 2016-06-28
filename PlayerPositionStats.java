5844d0f9b3bca54bb89fb7587a6145708081b9e2
Yagyaansh <yg6uk@virginia.edu>
2016-06-28 09:09:44 +0800

    Update PlayerPositionStats.java

--- PlayerPositionStats.java
+++ PlayerPositionStats.java
@@ -1,5 +1,86 @@
-public PlayerPositionStats {
+public class PlayerPositionStats {
   
-  private 
+  private int secondaryScore;
+  private int linebackerScore;
+  private int defensivelineScore;
+  private int offensivelineScore;
+  private int receiverScore;
+  private int runningbackScore;
+  
+  public PlayerPositionStats()
+  {
+	  this.secondaryScore = 0;
+	  this.linebackerScore = 0;
+	  this.defensivelineScore = 0;
+	  this.offensivelineScore = 0;
+	  this.receiverScore = 0;
+	  this.runningbackScore = 0;
+  }
+
+  
+ /*
+ * Getters and setters for the fields in the class
+ * -------------------------------------------------------------------------
+ * ------------------------
+ */
+  
+  public int getSecondaryScore() {
+	return secondaryScore;
+  }
+
+  public void setSecondaryScore(int secondaryScore) {
+	this.secondaryScore = secondaryScore;
+  }
+
+  public int getLinebackerScore() {
+	return linebackerScore;
+  }
+
+  public void setLinebackerScore(int linebackerScore) {
+	this.linebackerScore = linebackerScore;
+  }
+
+  public int getDefensivelineScore() {
+	return defensivelineScore;
+  }	
+
+  public void setDefensivelineScore(int defensivelineScore) {
+	this.defensivelineScore = defensivelineScore;
+  }
+
+  public int getOffensivelineScore() {
+	return offensivelineScore;
+  }
+
+  public void setOffensivelineScore(int offensivelineScore) {
+	this.offensivelineScore = offensivelineScore;
+  }
+
+  public int getReceiverScore() {
+	return receiverScore;
+  }
+
+  public void setReceiverScore(int receiverScore) {
+	this.receiverScore = receiverScore;
+  }
+
+  public int getRunningbackScore() {
+	return runningbackScore;
+  }
+
+  public void setRunningbackScore(int runningbackScore) {
+	this.runningbackScore = runningbackScore;
+  }
+  
+  public getBestPosition()
+  {
+    // the position with the highest score will be returned 
+  }
+  
+ /*
+ * End of Getters and Setters
+ * -------------------------------------------------------------------------
+ * ------------------------
+ */
 
 }
