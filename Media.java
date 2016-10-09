6cec8fd88e7bf4190eeeaceaa6900382bc65219c
Yagyaansh <yagyaansh@d-172-25-99-24.dhcp.virginia.edu>
2016-10-09 16:46:45 -0400

    Media
    Updated logic for calculating media revenue for each team

--- Media.java
+++ Media.java
@@ -4,16 +4,16 @@ public class Media
 	 * All funds are annually updated and given to the teams
 	 */
 	private double totalBaseFunds; // divided equally for all teams
-	private double totalAdvertisementFunds;
-	private double totalLicensingFunds;
+	private double advertisementFund;
+	private double licensingFund;
 	private int numberOfTeams;
 	
 	
 	public Media()
 	{
 		this.totalBaseFunds = 100;
-		this.totalAdvertisementFunds = 100;
-		this.totalLicensingFunds = 100;
+		this.advertisementFund = 100;
+		this.licensingFund = 100;
 		this.numberOfTeams = Main.numberOfTeams();
 	}
 	
@@ -28,8 +28,47 @@ public class Media
 		 * media coverage and licensing
 		 */
 		
+		double baseRevenue = totalBaseFunds / numberOfTeams;
+		double advertisementRevenue = getAdvertisementRevenue(team);
+		double licensingRevenue = getLicensingRevenue(team);
 		
+		revenue = baseRevenue + advertisementRevenue + licensingRevenue;
 		
+		return revenue;
+	}
+
+	private double getLicensingRevenue(Team team) {
+		
+		double revenue = 0.0;
+		
+		double factor = 1.0;
+		
+		/*
+		 * TODO : 
+		 * Use the home town, population and stadium capacity
+		 * to calculate a number between -0.5 and +0.5
+		 * Store the value in factor
+		 */
+		
+		revenue = licensingFund + (licensingFund*factor);
+	
+		return revenue;
+	}
+
+	private double getAdvertisementRevenue(Team team) {
+		
+		double revenue = 0.0;
+		
+		double factor = 1.0;
+		
+		/*
+		 * TODO : 
+		 * Use the current team form to calculate a number between -0.5 and +0.5
+		 * Store the value in factor
+		 */
+		
+		revenue = advertisementFund + (advertisementFund*factor);
+	
 		return revenue;
 	}
 	
