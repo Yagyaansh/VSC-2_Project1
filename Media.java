4547e02a5f0e3c586d2ede900c442fd303499d4a
Yagyaansh <yagyaansh@d-172-25-99-24.dhcp.virginia.edu>
2016-10-09 16:19:48 -0400

    Media.java
    Updated logic for determining amount each team makes from media, ads
    and TV licensing.

--- Media.java
+++ Media.java
@@ -1,6 +1,45 @@
-
 public class Media 
 {
+	private double totalAnnualFunds;
 	
+	public Media()
+	{
+		this.setTotalAnnualFunds(100000000);
+	}
 
+
+	/*
+	 * Getters and setters for the fields in the class
+	 * -------------------------------------------------------------------------
+	 * ------------------------
+	 */
+	
+	public double getTotalAnnualFunds() {
+		return totalAnnualFunds;
+	}
+
+	public void setTotalAnnualFunds(double totalAnnualFunds) {
+		this.totalAnnualFunds = totalAnnualFunds;
+	}
+	
+	/*
+	 * End of Getters and Setters
+	 * -------------------------------------------------------------------------
+	 * ------------------------
+	 */
+	
+	public double getMediaRevenueFor(Team team)
+	{
+		double revenue = 0.0;
+		
+		/*
+		 * TODO : 
+		 * Depending on the team, population, home town and how well its performing
+		 * We have to figure out how much each team makes from
+		 * media coverage and licensing
+		 */
+		
+		return revenue;
+	}
+	
 }
